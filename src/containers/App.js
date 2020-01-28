7822d9ce9006187872655a4d89bec6f140606dd1
Rajat <rajats1.kv2srg@gmail.com>
2020-01-29 01:24:29 +0530

    Add Homepage and Navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,15 @@ import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from './Navbar';
+import Main from './Main';
 const store = configureStore();
 
 const App = ()=> (
 	<Provider store={store}>
 		<Router>
-			<div className="onboarding">
+			<div className="onboarding ">
 				<Navbar />
+				<Main />
 			</div>
 		</Router>
 	</Provider>

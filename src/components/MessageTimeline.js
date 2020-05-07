1014c9f0481cdfa7ee9608900df7dca80c06414f
Rajat <rajats1.kv2srg@gmail.com>
2020-05-07 22:57:29 +0530

    UserAside Component

--- src/components/MessageTimeline.js
+++ src/components/MessageTimeline.js
@@ -1,9 +1,14 @@
 import React from "react";
 import MessageList from "../containers/MessageList";
+import UserAside from "./UserAside";
 
 const MessageTimeline  = props => {
 	return (
 		<div className="row">
+			<UserAside
+				proifleImageUrl={props.proifleImageUrl}
+				username={props.username}
+			/>
 			<MessageList />
 		</div>
 	);
